b68390493c72e99c20a50f7bac6e738cf92feade
jacksonflores <jacksonononon@gmail.com>
2023-07-06 16:03:13 -0600

    added magic cors shit?

--- backend/main.go
+++ backend/main.go
@@ -4,11 +4,16 @@ import (
 	"github.com/jacksonflores/todo-app/src/handlers"
 	"github.com/jacksonflores/todo-app/src/utils"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	utils.ParseSettings()
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000"},
+	}))
 
 	e.GET("/getTasks", handlers.GetTasks)
 
